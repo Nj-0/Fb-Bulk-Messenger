267b8502f29235c6158a390e3e61a020e4d5d968
Nj-0 <nammanjain@gmail.com>
2016-11-06 08:35:13 +0530

    full

--- messenger.py
+++ messenger.py
@@ -1,9 +1,54 @@
 import fbchat
+import re
 
-client = fbchat.Client("firefisher", "RustyCrimson")
+def getFriendListRaw():
 
-friends = client.getUsers("Revanth")  # return a list of names
-friend = friends[0]
-sent = client.send(friend.uid, "yo")
-if sent:
-    print("Message sent successfully!")
+	fileData = open('raw.txt', 'r')
+	friendFile = open('friend.txt', 'w')
+	friendList=[]
+	for line in fileData:
+
+	  matchFriendLiteral = re.search('[^\n]*[fF]riend', line)
+	  matchDesc=re.search('        ', line)
+
+	  if matchFriendLiteral or matchDesc :
+	  	continue
+	  elif line!='\n':
+	  	friendList.append(line.strip())
+		friendFile.write("%s\n" % line.strip())
+
+	print "Generated friends.txt"
+
+	return friendList
+
+def getFriendList():
+
+	fileData = open('friend.txt', 'r')
+	friendList=[]
+	for line in fileData:
+	  	friendList.append(line.strip())
+
+	return friendList
+
+
+try:
+    f = open('friend.txt')
+    friendList = getFriendList()
+except IOError:
+    try:
+    	f = open('raw.txt')
+    	friendList = getFriendListRaw()
+    except IOError:
+    	print 'Oh dear. Can not find the raw '
+    	exit()
+
+
+client = fbchat.Client("Your_Username", "Your_Password")
+
+for username in friendList:
+	friends = client.getUsers(username)  # return a list of names
+	if friends:
+		friend=friends[0].uid
+		sent = client.send(friend, "Hi :)")
+		if sent:
+			print "message sent to " + username
